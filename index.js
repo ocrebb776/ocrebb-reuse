d20d51a31ee7378a2ff9676e75b8d568a901551b
ocrebb776 <79861024+ocrebb776@users.noreply.github.com>
2022-02-12 15:50:41 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function setCookie(cname, cvalue, exdays) {
   const d = new Date();
   d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
   let expires = "expires="+d.toUTCString();
-  document.cookie = cname + "=" + cvalue + ";" + expires + ";SameSite=None; Secure";
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";SameSite=lax; Secure";
 }
 
 function getCookie(cname) {
